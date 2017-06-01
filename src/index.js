e711f0237e40e0effa3dc508ef3ddc8159fbb73c
Ionut Cirja <ionut.cirja@gmail.com>
2017-06-01 15:28:56 +0100

    refactoring / track method

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
-import init from './init';
+/* eslint-disable no-param-reassign, dot-notation */
+const loadGASDK = () => {
+    ((i, s, o, g, r, a, m) => {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function (...args) { (i[r].q = i[r].q || []).push(args); };
+        i[r].l = 1 * new Date();
+        a = s.createElement(o);
+        m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m);
+    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+};
+/* eslint-enable */
+
+const init = (trackingID, options) => {
+    loadGASDK();
+    window.ga('create', trackingID, options);
+};
+
+const track = (options) => {
+    window.ga('send', options);
+};
 
 export default {
     init,
+    track,
 };
---
+++ src/index.spec.js
@@ -0,0 +1,61 @@
+import chai, { expect } from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import ga from './';
+
+chai.use(sinonChai);
+
+describe('GoogleAnalytics api', () => {
+    const sandbox = sinon.sandbox.create();
+
+    beforeEach(() => {
+        window.ga = sandbox.stub();
+    });
+
+    afterEach(() => {
+        sandbox.restore();
+    });
+
+    describe('init', () => {
+        const gaScriptEl = {};
+        let insertNodeSpy;
+        let scriptEl;
+        const gaOptions = {
+        };
+
+        beforeEach(() => {
+            insertNodeSpy = sandbox.spy();
+            scriptEl = {
+                parentNode: {
+                    insertBefore: insertNodeSpy,
+                },
+            };
+            sandbox.stub(document, 'createElement');
+            document.createElement.withArgs('script').returns(gaScriptEl);
+            sandbox.stub(document, 'getElementsByTagName').returns([scriptEl]);
+        });
+
+        it('should create a script element that starts asynchronously downloading the analytics and ' +
+        'initialise a global ga function', () => {
+            ga.init('123', gaOptions);
+            expect(document.createElement).to.have.been.calledWith('script');
+            expect(document.getElementsByTagName).to.have.been.calledWith('script');
+            expect(insertNodeSpy).to.have.been.calledWith(gaScriptEl, scriptEl);
+            expect(gaScriptEl.async).to.equal(1);
+            expect(gaScriptEl.src).to.equal('https://www.google-analytics.com/analytics.js');
+            expect(window.ga).to.have.been.calledWith('create', '123', gaOptions);
+        });
+    });
+
+    describe('track', () => {
+        it('should call the global ga function', () => {
+            const trackOptions = {
+                hitType: 'pageview',
+                page: '/path',
+            };
+            ga.track(trackOptions);
+
+            expect(window.ga).to.have.been.calledWith('send', trackOptions);
+        });
+    });
+});
